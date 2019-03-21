1be83e4ed469e119b61feb8de7e571bd4c0d6c99
penny2252 <18643112252@163.com>
2019-03-21 11:52:54 +0800

    Initial commit

---
+++ 第一章.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array([1,2,3])
+print(x)
+print(len(x))
+print(x[0])
+print(x[0]+3)
+print(x[:2])
+print(x.dtype)
+
+print(x)
+print('*'*10)
+x=np.arange(0,6,0.1)
+y1=np.sin(x)
+y2=np.cos(x)
+plt.plot(x,y1,label='sin')
+plt.plot(x,y2,linestyle='--',label='cos')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('sin&cos')
+plt.legend()
+plt.show()
