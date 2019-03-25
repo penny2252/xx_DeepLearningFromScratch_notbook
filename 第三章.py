e1fe6623999bd2ed7e61a874ff725a6cc1067403
penny2252 <18643112252@163.com>
2019-03-25 12:44:30 +0800

    nothing

---
+++ 第三章.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array([2,-2,1,0])
+y=x>0
+print(y)
+y=y.astype(np.int)
+print(y)
+y=y.astype(np.bool)
+print(y)
+x=x.astype(np.bool)
+print(x)
+x=x.astype(np.int)
+print(x)
+
+print('-'*10)
+def step_functon(x):
+    return np.array(x>0,dtype=np.int)
+x=np.arange(-5,5,0.1)
+y1=step_functon(x)
+plt.plot(x,y1)
+# plt.ylim(-0.1,1.1)
+# plt.show()
+
+print('-'*10)
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+y2=sigmoid(x)
+plt.plot(x,y2)
+# plt.ylim(-0.1,1.1)
+# plt.show()
+print('-'*10)
+
+def relu(x):
+    return np.maximum(0,x)
+y3=relu(x)/5
+plt.plot(x,y3)
+plt.ylim(-0.1,1.1)
+plt.show()
